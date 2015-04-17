17be131131cf797dcb72da35b8aedc5aded00709
Jeremy Clemans <jeremy@notch8.com>
2015-04-17 12:14:53 -0700

    adds 3 passing tests

--- app/models/haiku.rb
+++ app/models/haiku.rb
@@ -1,3 +1,4 @@
 class Haiku < ActiveRecord::Base
   has_and_belongs_to_many :words
+  validates :content, presence: true
 end
--- spec/api/v1/haikus_spec.rb
+++ spec/api/v1/haikus_spec.rb
@@ -5,10 +5,14 @@ describe Haiku do
     @haiku = Haiku.new(content: "hello world")
   end
 
+  subject { @haiku }
+  
   it { should respond_to :content }
   it { should be_valid }
 
   describe "when no content is present" do
-    before { haiku.content = "" }
+    before { @haiku.content = "" }
     it { should_not be_valid }
   end
+
+end
